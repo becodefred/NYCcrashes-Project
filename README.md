33f6db8a038aa73efbee2325dee392d9357858f6
becodefred <fourrefrederic593@gmail.com>
2020-12-17 12:37:06 +0100

    Files from Fred: initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+###1. Background
+
+Name of the Project: Data preprocessing - NYC Motor Vehicle Crashes
+Context of the study: BeCode, Liège Campus, AI/Data Operator Bootcamp, December 2020
+Objective: apply preprocessing techniques to a dataset (.csv file)
+Author: Frédéric Fourré
+Email: fourrefrederic593@gmail.com
+
+
+###2.Dataset
+
+Source: New York City (NYC) OpenData (see https://data.cityofnewyork.us)
+Title: Motor Vehicle Collisions - Crashes
+Initial sample shape: 100000 rows, 29 columns
+Description: each row represents a crash event. A few column names: &quot;crash date&quot;, &quot;zip code&quot;, &quot;number of persons injured&quot;, &quot;contributing factor vehicle 1&quot;, &quot;vehicle type code 1&quot;. Full description available at https://data.cityofnewyork.us
+
+Data in our sample were collected between 23/03/2013 and 29/09/2020
+
+Name of the .csv file containing the original data: data_100000.csv
+
+
+###3. Tools/Keywords
+
+GeoPy, NumPy, Pandas
+
+
+###4. To run the file
+
+From terminal, type `python3`, then type `exec(open('crashesfred.py').read())`
+
+
+###5. Preprocessing steps
+
+Initial shape of the DataFrame is (100000, 29)
+
+5.1 Rename the columns to facilitate inspection of the DataFrame: &quot;date&quot;, &quot;time&quot;, &quot;borough&quot;, &quot;zipcode&quot;, &quot;latitude&quot;, &quot;longitude&quot;, &quot;location&quot;, &quot;onstreet&quot;, &quot;offstreet&quot;, &quot;crossstreet&quot;, &quot;totinjured&quot;, &quot;totkilled&quot;, &quot;pedinjured&quot;, &quot;pedkilled&quot;, &quot;cycinjured&quot;, &quot;cyckilled&quot;, &quot;motinjured&quot;, &quot;motkilled&quot;, &quot;factorveh1&quot;, &quot;factorveh2&quot;, &quot;factorveh3&quot;, &quot;factorveh4&quot;, &quot;factorveh5&quot;, &quot;id&quot;, &quot;typeveh1&quot;, &quot;typeveh2&quot;, &quot;typeveh3&quot;, &quot;typeveh4&quot;, &quot;typeveh5&quot;
+
+5.2 Put crash events in chronological order, and add columns &quot;year&quot;, &quot;month&quot;, &quot;day&quot;, &quot;hour&quot; and &quot;minute&quot;
+
+5.3 There were missing values (NA in Pandas) for &quot;latitude&quot;, &quot;longitude&quot; and &quot;location&quot;. We made geocoding tests to see if it was possible to retrieve latitude and longitude from &quot;borough&quot;, &quot;zipcode&quot;, &quot;onstreet&quot; and &quot;offstreet&quot;. The answer is NO, in general, due to mechanical errors in writing which are unpredictable
+
+Remove rows for which location of the crash is not available. Also remove rows with latitude and longitude equal to 0
+
+5.4 Remove rows for which (factorveh1 = NA AND factorveh2 = NA AND etc. AND factorveh5 = NA) 
+
+5.5 Replace NaN and &quot;Unspecified&quot; values in factorvehi (i = 1, 2, 3, 4, 5) by 0
+
+5.6 Replace NaN values in &quot;borough&quot;, &quot;zipcode&quot;, &quot;onstreet&quot;, &quot;offstreet&quot; and &quot;crossstreet&quot; by 0
+
+5.7 Processing typevehi (i = 1, 2, 3, 4, 5)
+
+5.7.1 Find unique values in typevehi and convert these to lower case.
+
+5.7.2 Find unique values containing string &quot;tow&quot; (examples: TOW, Tow truck), apply &quot;tow filter&quot; to typevehi (i = 1, 2, 3, 4, 5) and replace the name of any occurence with name &quot;Tow&quot;
+
+5.7.3 Repeat the process described in 5.7.2 with the following filters: &quot;amb&quot; (Ambulance), &quot;trac&quot; (Tractor Truck), &quot;fir&quot; (Fire Truck), &quot;sco&quot; (Scooter), &quot;pic&quot; (Pickup), &quot;motor&quot; (Motorcycle), &quot;bik&quot; (Bike), &quot;dum&quot; (Dump Truck), &quot;tax&quot; (Taxi), &quot;box&quot; (Box Truck), &quot;van&quot; (Van), &quot;bus&quot; (Bus), &quot;trail&quot; (Trailer), &quot;seda&quot; (Sedan), &quot;fork&quot; (Forklift)
+
+5.7.4 Apply miscellaneous filters to get the following unique values: Limousine, Truck, Mail Truck, Station Wagon/Sport Utility Vehicle, Garbage or Refuse
+
+5.7.5 Remove garbage or insignificant values in typevehi (examples: OTHER, UNKNOWN, pc)
+
+5.7.6 Replace NaN values in typevehi (i = 1, 2, 3, 4, 5) with 0
+
+5.8 Look for duplicates
+
+5.9 Create tuples from &quot;latitude and &quot;longitude&quot; and put them in a new column named &quot;location2&quot;
+
+5.10 Reset index of DataFrame
+
+5.11 Reverse geocoding: from coordinates in &quot;location2&quot; find values for borough, zipcode and street, and put the results in &quot;borough2&quot;, &quot;zipcode2&quot; and &quot;onstreet2&quot;. 
+
+Used to check the data in the original dataset or replace missing values. 
+
+Final shape of DataFrame is (90779, 39)
+
+5.12 Save the final DataFrame in a .csv file: see datcrashesfred.csv
+
+
+###6. Improvements/To do
+
+For 5.7: 
+
+- use a For loop to sort vehicle types
+- other sample datasets may contain other garbage or insignificant strings (mechanical mistakes in written language are unpredictable) and new vehicle types. The program should at least be able to detect such modifications
+- check the meaning of some vehicle types (example: PK)
+
+Compare the results obtained from reverse geocoding to the original data (borough, zipcode, onstreet)
+
+
+
+
+
+
+
+
---
+++ crashesfred.py
@@ -0,0 +1,332 @@
+# Name of the Project: Data preprocessing - NYC Motor Vehicle Crashes
+# Context of the study: BeCode, Liège Campus, AI/Data Operator Bootcamp, December 2020
+# Objective: apply preprocessing techniques to a dataset (.csv file)
+# Author: Frédéric Fourré
+# Email: fourrefrederic593@gmail.com
+# To run this file in a terminal: type python3, then exec(open('crashesfred.py').read())
+
+
+import pandas as pd
+import numpy as np
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+
+
+input_file = 'data_100000.csv'
+dat = pd.read_csv(input_file, header = 0)
+
+# initial shape of the DataFrame is (100000, 29)
+dat.shape
+# initial dtypes in the DataFrame
+dat.dtypes
+# put the original column names in a python list
+original_headers = list(dat.columns.values)
+
+# rename columns to facilitate inspection of the DataFrame
+dat.columns = ['date', 'time', 'borough', 'zipcode', 'latitude', 'longitude', 'location', 'onstreet', 'offstreet', 'crossstreet', 'totinjured', 'totkilled', 'pedinjured', 'pedkilled', 'cycinjured', 'cyckilled', 'motinjured', 'motkilled', 'factorveh1', 'factorveh2', 'factorveh3', 'factorveh4', 'factorveh5', 'id', 'typeveh1', 'typeveh2', 'typeveh3', 'typeveh4', 'typeveh5']
+
+headers = list(dat.columns.values)
+
+# put crash events in chronological order
+dat.date = pd.to_datetime(dat.date)
+dat.time = pd.to_timedelta(dat.time+':00')
+# concatenate
+dat.date = pd.to_datetime(dat["date"] + dat["time"])
+# sort
+dat.sort_values(by = ["date"], inplace = True)
+
+# split date and time, and add columns "year", "month", "day", "hour", "minute"
+k = 2
+dat.insert(k, "year", dat['date'].dt.year)
+dat.insert(k + 1, "month", dat['date'].dt.month)
+dat.insert(k + 2, "day", dat['date'].dt.day)
+dat.insert(k + 3, "hour", pd.to_datetime(dat.time).dt.hour)
+dat.insert(k + 4, "minute", pd.to_datetime(dat.time).dt.minute)
+
+# detect missing values
+dat.isnull().sum()
+
+# remove rows for which location of the crash is not available
+dat.dropna(axis = 0, how = 'any', subset = ['latitude', 'longitude', 'location'], inplace = True)
+
+# remove rows with latitude AND longitude equal to 0
+dat=dat[~((dat.latitude == 0) & (dat.longitude == 0))]
+# shape is now (91796, 34)
+
+# remove rows for which (factorveh1=NA AND factorveh2=NA AND etc. AND factorveh5=NA) 
+cols1 = ['factorveh1', 'factorveh2', 'factorveh3', 'factorveh4', 'factorveh5']
+dat.dropna(axis = 0, how = 'all', subset = cols1, inplace = True)
+# shape is now (91442, 34)
+
+# replace NaN and "Unspecified" values in factorvehi (i = 1, 2, 3, 4, 5) by 0
+dat[cols1] = dat[cols1].fillna(0)
+dat[cols1] = dat[cols1].replace('Unspecified', 0)
+
+# replace NaN in 'borough', 'zipcode', 'onstreet', 'offstreet' and 'crossstreet' by 0
+cols2 = ['borough', 'zipcode', 'onstreet', 'offstreet', 'crossstreet']
+dat[cols2] = dat[cols2].fillna(0)
+
+# processing typevehi (i = 1, 2, 3, 4, 5)
+# find unique values in typevehi and convert these to lower case
+cols3 = ["typeveh1", "typeveh2", "typeveh3", "typeveh4", "typeveh5"]
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+
+# find any unique value containing string "tow" and display the results
+sub = 'tow'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+
+# check if filter RegEx works for filt object
+pd.Series(filt).str.replace('^([tT][oO][wW]).+', 'Tow', regex = True)
+# apply the filter to typevehi (i = 1, 2, 3, 4, 5)
+dat[cols3] = dat[cols3].replace('^([tT][oO][wW]).+', 'Tow', regex = True)
+
+# check "tow" cleaning for column "typeveh1"
+dat["typeveh1"].value_counts()
+
+# filter for Ambulance
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'amb'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([aA][mM][bB]).*', 'Ambulance', regex=True)
+
+# filter for Tractor Truck
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'trac'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([tT][rR][aA][cC]).*', 'Tractor Truck', regex=True)
+
+# filter for Fire Truck
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'fir'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([fF][iI][rR]).*', 'Fire Truck', regex=True)
+
+
+# filter for Scooter
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'sco'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([sS][cC][oO]).*', 'Scooter', regex=True)
+
+# filter for Pickup
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'pic'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([pP][iI][cC]).*', 'Pickup', regex=True)
+
+# filter for Motorcycle
+xval = dat[cols3].values.ravel()
+x = pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'motor'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+
+dat[cols3] = dat[cols3].replace('MOTORCYCLE', 'Motorcycle', regex = True)
+dat[cols3] = dat[cols3].replace('motorcycle', 'Motorcycle', regex = True)
+dat[cols3] = dat[cols3].replace('Motorbike', 'Motorcycle', regex = True)
+dat[cols3] = dat[cols3].replace('MOPED', 'Motorcycle', regex = True)
+dat[cols3] = dat[cols3].replace('moped', 'Motorcycle', regex = True)
+dat[cols3] = dat[cols3].replace('Moped', 'Motorcycle', regex = True)
+
+# filter for Bike
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'bik'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([bB][iI][kK]).*', 'Bike', regex = True)
+
+# filter for Dump Truck
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'dum'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([dD][uU][mM]).*', 'Dump Truck', regex = True)
+
+# filter for Taxi
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'tax'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('TAXI', 'Taxi', regex = True)
+
+# filter for Box Truck
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'box'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([bB][oO][xX]).*', 'Box Truck', regex = True)
+
+# filter for Van
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'van'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([vV][aA][nN]).*', 'Van', regex = True)
+
+# filter for Bus
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'bus'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([bB][uU][sS]).*', 'Bus', regex = True)
+
+# filter for Trailer
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'trail'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([tT][rR][aA][iI][lL]).*', 'Trailer', regex = True)
+
+# filter for Sedan
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'seda'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([sS][eE][dD][aA]).*', 'Sedan', regex = True)
+
+
+# filters for miscellaneous
+dat[cols3] = dat[cols3].replace('SPORT UTILITY / STATION WAGON', 'Station Wagon/Sport Utility Vehicle', regex = True)
+dat[cols3] = dat[cols3].replace('GARBAGE TR', 'Garbage or Refuse', regex = True)
+
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'limo'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+
+dat[cols3] = dat[cols3].replace('LIMOU', 'Limousine', regex = True)
+dat[cols3] = dat[cols3].replace('LIMO', 'Limousine', regex = True)
+dat[cols3] = dat[cols3].replace('limo', 'Limousine', regex = True)
+
+dat[cols3] = dat[cols3].replace('MAIL TRUCK', 'Mail Truck', regex = True)
+dat[cols3] = dat[cols3].replace('mailtruck', 'Mail Truck', regex = True)
+dat[cols3] = dat[cols3].replace('MAILTRUCK', 'Mail Truck', regex = True)
+
+dat[cols3] = dat[cols3].replace('TRUCK', 'Truck', regex = True)
+dat[cols3] = dat[cols3].replace('truck', 'Truck', regex = True)
+
+# filter for Forklift
+xval = dat[cols3].values.ravel()
+x =  pd.unique(xval)
+y = list(pd.Series(x).str.lower())
+sub = 'fork'
+z = np.core.defchararray.find(y, sub, start = 0, end = None)
+filt = x[z >= 0]
+dat[cols3] = dat[cols3].replace('.*([fF][oO][rR][kK]).*', 'Forklift', regex = True)
+
+
+# remove garbage or insignificant values in typevehi
+# identify all vehicle types, sum values and sort types
+tab = dat[cols3].apply(pd.Series.value_counts)
+indextab = list(tab.index)
+filt3count = tab.sum(axis = 1)
+filt3 = filt3count.sort_values(axis = 0, ascending = False)
+indexfilt3 = list(filt3.index)
+
+# keep 34 vehicle types 
+keep = ['Sedan', 'Station Wagon/Sport Utility Vehicle', 'Taxi', 'Pickup', 'Box Truck', 'Bike', 'Bus', 'Motorcycle', 'Tractor Truck', 'Van', 'Ambulance', 'Scooter', 'Convertible', 'Dump Truck', 'PK', 'Garbage or Refuse', 'Flat Bed', 'Carry All', 'Tow', 'Chassis Cab', 'Fire Truck', 'Tanker', 'Concrete Mixer', 'Flat Rack', 'Lift Boom', 'Armored Truck', 'Trailer', 'Beverage Truck', 'Limousine', 'Multi-Wheeled Vehicle', 'Truck', 'Stake or Rack', 'Forklift', 'Mail Truck']
+# remove 328 vehicle types 
+rem = ['3-Door', 'PASSENGER VEHICLE', 'Open Body', 'UNKNOWN', 'UNK', 'FDNY', 'USPS', 'DELV', 'Bulk Agriculture', 'Pedicab', 'UNKNO', 'FDNY Truck', 'REFG', 'UTIL', 'Minicycle', 'DELIV', 'Unknown', 'Lunch Wagon', 'COMMERCIAL', 'unkno', 'SCHOO', 'POWER SHOV', 'POSTAL TRU', 'COM', 'MTA B', 'unknown', 'PAS', 'USPS Truck', 'ELECT', 'Pallet', 'usps', 'unk', 'STREE', 'GOLF CART', 'US PO', 'PSD', 'POSTA', 'FLATBED', 'TRK', 'MACK', 'DELIVERY T', 'Motorized Home', 'ICE CREAM', 'BACKH', 'commercial', 'Well Driller', 'COMME', 'wagon', 'FLAT', 'TRL', 'ELECTRIC S', 'Elect', 'Enclosed Body - Nonremovable Enclosure', 'Schoo', 'SUV', 'elect', 'FDNY EMS', 'RV', 'ROAD SWEEP', 'fdny', 'government', 'GARBA', 'hrse', 'Hopper', 'FOOD', 'postal tru', 'BOBCA', 'PC', 'LIGHT TRAI', 'sanitation', 'OTHER', 'Glass Rack', 'MOVIN', 'BULLD', 'Unkno', 'SANITATION', 'back', 'WORK', 'Delv', 'Delivery t', 'UHAUL', 'DELIVERY V', 'bobca', 'UT', 'USPS POSTA', 'DELIVERY', 'FREIGHTLIN', 'ASPHA', 'ACCESS A R', 'Flatbed', 'Flat bed', 'ALUMI', 'AMAZON SPR', 'BOB CAT', 'APPOR', 'BACK HOE', 'FRIEGHTLIN', 'Commercial', 'FREIGHT FL', 'DETAC', 'Flat', 'BLACK', 'FUSION', 'FREIGHT TR', 'BACKHOE LO', 'Fleet', 'Cement Tru', 'Const', 'Horse', 'HORSE', 'HI TA', 'HEARSE', 'HAUL FOR H', 'HAND', 'Golf Cart', 'Golf', 'Go kart', 'Courier', '18 WEELER', 'Ford sprin', '18 WHEELER', 'GLP050VXEV', '35 FT', 'GLNEN', '4D', '4DS', 'GATOR', 'Front-Load', 'Freight', '4dsd', 'DIRTB', 'E450', 'FREIG DELV', 'ENGIN', 'F550', 'E-SKA', 'CAT32', 'CHEVROLET', 'Enclosed Body - Removable Enclosure', 'COURIER', 'Electric s', 'CHEVY EXPR', 'CHURC', 'E-SKATEBOA', 'ENCLO', 'FD Truck', 'EMS Truck', 'EMS', 'E-UNICYCLE', 'CITY', 'E350', 'ELECTRIC M', 'CONT', 'COMMERICAL', 'CONCRETE M', 'ELEC. UNIC', 'FD LADDER', 'E REVEL SC', 'FREIG', 'FLAT BED', 'INTERNATIO', 'BOBCAT FOR', 'FOOD Truck', 'BS', 'BTM', 'DRILL RIG', 'FLATBED TR', 'Deliv', 'FLATBED FR', 'Bucket Tru', 'Can', 'CRANE', 'Depar', 'Crane', 'CAMPE', 'FED E', 'CAT', 'FDNY LADDE', 'FDNY Engin', 'FDNY ENGIN', 'FDNY #226', 'CAT.', 'DOT EQUIPM', 'yello', 'INTL', 'IP', 'Wagon', 'Wh Ford co', 'almbulance', 'backhoe', 'boom', 'boom lift', 'com', 'comm.', 'const', 'constructi', 'crane', 'cross', 'dark color', 'deliv', 'dilevery t', 'WINEB', 'WHITE', 'VEHICLE 2', 'USPOS', 'UHAUL Trai', 'UKN', 'UNATTACHED', 'UNKN', 'UNKNOWN VE', 'UPS Truck', 'USPS/GOVT', 'Utility.', 'UTILI', 'UTILITY', 'UTILITY TR', 'UTILITY VE', 'Unk', 'Utili', 'dp', 'e skate bo', 'e-350', 'trlr', 'stree', 'street cle', 'suburban', 'tk', 'transport', 'trl', 'uhaul', 'spec-', 'uhaul truc', 'uknown', 'util', 'utili', 'utility', 'vespa', 'sprin', 'self', 'electric s', 'itas', 'excav', 'f-250', 'fdny ems', 'forlift', 'g spc', 'golf cart', 'message si', 'rep', 'mopd', 'motor', 'p/sh', 'pc', 'prks', 'range', 'U-HAUL', 'Truck TRAI', 'Truck FLAT', 'winne', 'MOBIL', 'MOBILE', 'MOTOR', 'MOTOR SKAT', 'MTA Truck', 'MTA b', 'Mta', 'MECHANICAL', 'NYC ACS Va', 'NYC FD', 'NYS A', 'OTH', 'P/SH', 'PALFINGER', 'MINI', 'MAIL', 'POLIC', 'LADDER TRU', 'JETSKI', 'JLG L', 'JOHN', 'JOHN DEERE', 'John Deere', 'LADDER 34', 'LCOM', 'Livestock Rack', 'LCOMM', 'LEFT THE S', 'LMA', 'Lawnmower', 'Lift', 'Light trai', 'PASS', 'POST', 'Tree cutte', 'TRA/R', 'Snow Plow', 'Sprin', 'Subn', 'Sweeper', 'TL', 'TOYOT', 'TRANS', 'Sanitation', 'TRLR', 'TUCK', 'Tilt tande', 'Toyota', 'Train', 'Trc', 'Skateboard', 'SWT', 'POWER', 'SELF', 'PU', 'Parce', 'Postal tru', 'Pumper', 'RAM', 'RDS', 'SKATEBOARD', 'SWEEPER', 'SKID LOADE', 'SLINGSHOT', 'SMALL COM VEH(4 TIRES) ', 'SMART CAR', 'SPECIAL PU', 'STREET SWE', '0']
+
+
+dat = dat[~dat[cols3].isin(rem).any(axis = 1, skipna = False)]
+# shape is now (90779, 34)
+
+# replace NaN values in typevehi with 0
+dat[cols3] = dat[cols3].fillna(0)
+dat.isnull().sum()
+
+# check for duplicates 
+dat.duplicated().any()
+
+# create tuples from "latitude and "longitude" and put them in new column "location2"
+dat['location2'] = dat[['latitude', 'longitude']].apply(tuple, axis = 1)
+#shape is now (90779, 35)
+
+# reverse geocoding
+geolocator = Nominatim(user_agent = "fourrefrederic593@gmail.com")
+reverse = RateLimiter(geolocator.reverse, min_delay_seconds = 1)
+
+# reset index (a new column "index" is created which contains the old indexes)
+dat = dat.reset_index()
+#shape is now (90779, 36)
+
+#index will be [a, a+1, etc., b-1]
+a = 0
+b = 1000
+datloop = dat.iloc[a:b, :]
+
+i = 0
+#for row in dat[["location2"]].head(n = 10).itertuples():
+for row in datloop.itertuples():
+    infoloc = reverse(row.location2)      
+    if "road" in infoloc.raw["address"]:
+        dat.loc[row.Index, 'onstreet2'] = infoloc.raw["address"]["road"]
+    if "suburb" in infoloc.raw["address"]:  
+        dat.loc[row.Index, 'borough2'] = infoloc.raw["address"]["suburb"]
+    if "postcode" in infoloc.raw["address"]:   
+        dat.loc[row.Index, 'zipcode2'] = infoloc.raw["address"]["postcode"]          
+    i += 1
+    print(i)
+
+
+# save DataFrame to a .csv file
+dat.to_csv("datcrashesfred.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
